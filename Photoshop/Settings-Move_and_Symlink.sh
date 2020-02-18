be06254188a71b6d3d48a6c17e7d5fd62404a1f8
Basil Skrnk <trwl.Basil.Skrnk@gmail.com>
2020-02-18 20:51:32 +0200

    Add script for Photoshop Settings: Backup and Symlink

---
+++ Photoshop/Settings-Move_and_Symlink.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Script to ease syncronization of settings in Adobe Photoshop, on macOS
+# Designed with different users on one system in mind: either to install 
+# You need to run the script just once per each user, per one installation of Photoshop
+# In case of reinstalling, you have to repeat the script
+
+
+# Variables
+
+path_to_configs=/Configs/Adobe/PS/Settings/
+path_to_settings=${HOME}/Library/Preferences/Adobe\ Photoshop\ 2020\ Settings/
+path_to_presets=${HOME}/Library/Application\ Support/Adobe/Adobe\ Photoshop\ 2020/Presets/
+
+# Moving
+
+# Interactive to add: 
+# if: Synced Settings are present
+# then do backup of originals, instead of copying them
+path_to_configs_backup="$path_to_configs"
+path_to_configs="$path_to_configs"/Originals-CC-2020
+mkdir "$path_to_configs"
+# else: if there's no Synced Settings, then skip this step
+
+mv "$path_to_settings"/WorkSpaces "$path_to_configs"
+mv "$path_to_settings"/WorkSpaces\ \(Modified\) "$path_to_configs"
+mv "$path_to_settings"/Keyboard\ Shortcuts.psp "$path_to_configs"
+mv "$path_to_settings"/Keyboard\ Shortcuts\ Primary.psp "$path_to_configs"
+mv "$path_to_settings"/Actions\ Palette.psp "$path_to_configs"
+mv "$path_to_settings"/Patterns.psp "$path_to_configs"
+
+mv "$path_to_presets"/Keyboard\ Shortcuts "$path_to_configs"
+
+# and then restore path_to_configs to its original form, before Symlinking step
+path_to_configs="$path_to_configs_backup"
+
+# Symlinking 
+
+ln -s "$path_to_configs"/WorkSpaces "$path_to_settings"
+ln -s "$path_to_configs"/WorkSpaces\ \(Modified\) "$path_to_settings"
+ln -s "$path_to_configs"/Keyboard\ Shortcuts.psp "$path_to_settings"
+ln -s "$path_to_configs"/Keyboard\ Shortcuts\ Primary.psp "$path_to_settings"
+ln -s "$path_to_configs"/Actions\ Palette.psp "$path_to_settings"
+ln -s "$path_to_configs"/Patterns.psp "$path_to_settings"
+ln -s "$path_to_configs"/Keyboard\ Shortcuts "$path_to_presets"
